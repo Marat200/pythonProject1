ac0471525ddd64a6f388e0d6e604d33532c7febd
Marat <marat07052002@inbox.ru>
2020-11-25 14:19:01 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#print("Укажите фамилию: ")
+#surname = input()
+#print("Укажите возраст: ")
+#age = int(float(input()))
+#print("Возраст: " , age)
+#print("Был ли тест: ")
+#covid_test = int(bool(input()))
+#print("covid test: ", covid_test)
+
+days_in_month = 31
+days_in_week = 7
+print("Weeks in month:", days_in_month // days_in_week)
